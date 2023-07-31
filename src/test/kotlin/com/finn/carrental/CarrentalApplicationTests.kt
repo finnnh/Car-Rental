523024bf9da1e409da1bad848e07171c6c22d84a
Finn <fihoffmann@web.de>
2023-07-31 10:25:53 +0200

    First commit

---
+++ src/test/kotlin/com/finn/carrental/CarrentalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finn.carrental
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarrentalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
